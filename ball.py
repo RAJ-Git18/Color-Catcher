feef5a0bfd086355417f5d3bfa9b608ea18bf9dd
RAJ-Git18 <simkhadaraj98@gmail.com>
2024-06-08 15:04:14 +0545

    bouncy ball

--- ball.py
+++ ball.py
@@ -1,1 +1,14 @@
-#This file is for our balls
+import pygame
+
+
+# class to create a ball
+class Ball:
+    def __init__(self, screen, color, radius):
+        self.screen = screen
+        self.color = color
+        self.radius = radius
+
+    def draw_ball(self, x, y):
+        self.x = x
+        self.y = y
+        pygame.draw.circle(self.screen, self.color, (self.x, self.y), self.radius)
