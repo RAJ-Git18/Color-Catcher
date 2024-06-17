039956e20de32152a1c9d96df61aca7a6efb47f1
RAJ-Git18 <simkhadaraj98@gmail.com>
2024-06-17 13:50:43 +0545

    added the background music and chnage main file with the startpage file

--- main.py
+++ main.py
@@ -1,54 +1,63 @@
 import pygame
-import ball
+import game
 
-def game():
-    background_colour = (234, 212, 252)
-    
-    screen = pygame.display.set_mode((800, 600), pygame.RESIZABLE)
-    
-    pygame.display.set_caption("Color Balls")
-    
-    ball_position_x = 400
-    ball_position_y = 500
-    
-    # objects of the ball
-    ball_purple = ball.Ball(screen, background_colour, 25)
-    
-    # to make the window run
-    running = True
-    
-    ball_up = True
-    
-    # game loop
-    while running:
-        screen.fill(color="black")
-        # for loop through the event queue
-        for event in pygame.event.get():
-        
-            # Check for QUIT event
-            if event.type == pygame.QUIT:
+# Initialize Pygame
+pygame.init()
+
+# Set up the screen dimensions
+screen_width = 1550
+screen_height = 800
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Color-Switch Menu")
+
+# Load the background image
+background_image = pygame.image.load("background_img.jpg").convert()
+
+# load the background music
+background_music = pygame.mixer.music.load("sounds/bgmusic.mp3")
+pygame.mixer.music.play(-1)
+pygame.mixer.music.set_volume(0.05)
+
+whiteColor = (255, 255, 255)
+
+
+# Game text to be displayed on the screen
+font1 = pygame.font.Font("font/myfont.ttf", 60)
+gameName = font1.render("COLOR-SWITCH GAME", True, whiteColor)
+
+# start text to be displayed on the screen
+font2 = pygame.font.Font("font/myfont.ttf", 48)
+start_text = font2.render("START", True, whiteColor)
+start_box = start_text.get_rect(center=(800, 250))
+
+# exit text to be displayed on the screen
+font3 = pygame.font.Font("font/myfont.ttf", 48)
+exit_text = font3.render("EXIT", True, whiteColor)
+exit_box = exit_text.get_rect(center=(800, 350))
+
+# Scale the background image to fit the screen
+background_image = pygame.transform.scale(
+    background_image, (screen.get_width(), screen.get_height())
+)
+
+# Main game loop
+running = True
+while running:
+
+    screen.blit(background_image, (0, 0))
+
+    screen.blit(gameName, (500, 100))
+    screen.blit(start_text, start_box)
+    screen.blit(exit_text, exit_box)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if exit_box.collidepoint(event.pos):
                 running = False
-    
-            # elif event.type == pygame.KEYDOWN:
-            #     if event.key == pygame.K_UP:
-            #         while ball_position_y != 300:
-            #             ball_position_y -= 1
-            #             ball_purple = ball.Ball(
-            #                 screen, background_colour, ball_position_x, ball_position_y, 25
-            #             )
-    
-            #             print(ball_position_y)
-        if ball_up:
-            if ball_position_y > 300:
-                ball_position_y -= 0.2
-            else:
-                ball_up = False
-        elif ball_position_y <= 500:
-            ball_up = False
-            ball_position_y += 0.2
-    
-        # drawing the purple ball
-        ball_purple.draw_ball(ball_position_x, ball_position_y)
-    
-        pygame.display.update()
-    
+            if start_box.collidepoint(event.pos):
+                game.gameLoop()
+
+    # Update display
+    pygame.display.update()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,3 @@
-myfont.ttf
+background_image = pygame.transform.scale(
+#     background_image, (screen.get_width(), screen.get_height())
+# )
