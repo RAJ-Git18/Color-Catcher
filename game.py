6027917cd72c9fa043527864acc09835a39c6bd9
RAJ-Git18 <simkhadaraj98@gmail.com>
2024-07-12 21:58:51 +0545

    game.py updated

--- game.py
+++ game.py
@@ -1,54 +1,58 @@
 import pygame
 import ball
 
+pygame.init()
+
 
 def gameLoop():
+
     background_colour = (234, 212, 252)
-
     screen = pygame.display.set_mode((1550, 800), pygame.RESIZABLE)
-
     pygame.display.set_caption("Color Balls")
 
-    ball_position_x = 400
-    ball_position_y = 500
+    ball_position_x = 770
+    ball_position_y = 650
 
-    # objects of the ball
+    # Object of the ball
     ball_purple = ball.Ball(screen, background_colour, 25)
 
-    # to make the window run
+    # To make the window run
     running = True
 
-    ball_up = True
+    # To make the ball move up
+    up_position = False
+    down_position = False
 
-    # game loop
+    # Game loop
     while running:
         screen.fill(color="black")
-        # for loop through the event queue
-        for event in pygame.event.get():
 
+        # For loop through the event queue
+        for event in pygame.event.get():
             # Check for QUIT event
             if event.type == pygame.QUIT:
                 running = False
 
-            # elif event.type == pygame.KEYDOWN:
-            #     if event.key == pygame.K_UP:
-            #         while ball_position_y != 300:
-            #             ball_position_y -= 1
-            #             ball_purple = ball.Ball(
-            #                 screen, background_colour, ball_position_x, ball_position_y, 25
-            #             )
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    up_position = True
 
-            #             print(ball_position_y)
-        if ball_up:
+        if up_position:
             if ball_position_y > 300:
-                ball_position_y -= 0.2
+                ball_position_y -= 0.8
             else:
-                ball_up = False
-        elif ball_position_y <= 500:
-            ball_up = False
-            ball_position_y += 0.2
+                up_position = False
+                down_position = True
+        if down_position:
+            if ball_position_y < 650:
+                ball_position_y += 0.8
+            else:
+                down_position = False
 
-        # drawing the purple ball
+        # Drawing the purple ball
         ball_purple.draw_ball(ball_position_x, ball_position_y)
 
         pygame.display.update()
+
+
+gameLoop()
